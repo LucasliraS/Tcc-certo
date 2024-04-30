68d37acc8f0dfe9d06105da9f0be9ef7dbbb92e0
LucasliraS <lucaslira125@gmail.com>
2024-04-29 23:49:39 -0300

    primeiro commit

---
+++ Formulario/inserir_jogo.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Conexao com o banco de dados MySQL usando o XAMPP
+    $conexao = mysqli_connect("localhost", "root", "", "cadastro");
+
+    // Verifica se a conexao foi bem sucedida
+    if (mysqli_connect_errno()) {
+        echo "Falha ao conectar ao MySQL: " . mysqli_connect_error();
+        exit();
+    }
+
+    // Coleta os dados do formulário
+    $nome = $_POST['nome_do_jogo'];
+    $descricao = $_POST['descricao'];
+    $genero = $_POST['genero'];
+    $preco = $_POST['preco'];
+
+    // Prepara a consulta SQL para inserir os dados no banco de dados
+    $sql = "INSERT INTO Jogos (Nome, Descricao, Genero, Preco) VALUES ('$nome', '$descricao', '$genero', '$preco')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Novo jogo cadastrado com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . mysqli_error($conexao);
+    }
+
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conexao);
+}
+?>
